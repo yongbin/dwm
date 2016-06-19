c8005992bc06193e96f5a2a79e22d812ad831b74
Yongbin Yu <yongbin.yu@gmail.com>
2016-06-19 10:04:26 +0900

    Syns dwm from ABS
    1. abs communit/dwm
    2. cp /var/abs/community/dwm/* .

--- PKGBUILD
+++ PKGBUILD
@@ -1,65 +1,33 @@
-# $Id: PKGBUILD 113973 2014-07-01 10:51:04Z spupykin $
+# $Id: PKGBUILD 165985 2016-03-10 18:31:18Z spupykin $
 # Maintainer: Sergej Pupykin <pupykin.s+arch@gmail.com>
 # Contributor: Dag Odenhall <dag.odenhall@gmail.com>
 # Contributor: Grigorios Bouzakis <grbzks@gmail.com>
 
 pkgname=dwm
-pkgver=6.0
-pkgrel=2
+pkgver=6.1
+pkgrel=3
 pkgdesc="A dynamic window manager for X"
 url="http://dwm.suckless.org"
 arch=('i686' 'x86_64')
 license=('MIT')
 options=(zipman)
-depends=('libx11' 'libxinerama')
+depends=('libx11' 'libxinerama' 'libxft' 'freetype2' 'st' 'dmenu')
 install=dwm.install
 source=(http://dl.suckless.org/dwm/dwm-$pkgver.tar.gz
 	config.h
-	dwm.desktop
-	push.c
-	bstack.c
-	gaplessgrid.c
-	)
-_patches=(
-          01-statuscolours.diff
-          02-monoclecount.diff
-          03-noborder.diff
-          04-centredfloating.diff
-          05-scratchpad.diff
-          06-attachaside.diff
-         )
-source=(${source[@]} ${_patches[@]})
-md5sums=('8bb00d4142259beb11e13473b81c0857'
-         '3278e7aa44cd57cc24e23f8d63e04202'
-         '939f403a71b6e85261d09fc3412269ee'
-         '689534c579b1782440ddcaf71537d8fd'
-         '362e07f0f042875b84d7739d9d8855c4'
-         '4ba509b3b93f7b1418dc703c70de536f'
-         '57b1a8f21b61c55f906d7cc075111613'
-         'e3faeea09a554bbbce29c4d480b0ca41'
-         '1f0244803c0188f1b6f4e5794e7f5ca2'
-         'ed11483bfccbf65ff9714c0ca4e7bb23'
-         'bc6240f3adadf604a450f6375badec61'
-         'a92ee04c33b1082da61b55d3617249eb')
+	dwm.desktop)
+md5sums=('f0b6b1093b7207f89c2a90b848c008ec'
+         '80c4ef2a3eca0fe2d14e2203e3833200'
+         '939f403a71b6e85261d09fc3412269ee')
+
+prepare() {
+  cd $srcdir/$pkgname-$pkgver
+  cp $srcdir/config.h config.h
+}
 
 build() {
   cd $srcdir/$pkgname-$pkgver
-
-  for p in "${_patches[@]}"; do
-    echo "=> $p"
-    patch < ../$p || return 1
-  done
-
-  cp $srcdir/config.h config.h
-  cp $srcdir/push.c push.c
-  cp $srcdir/bstack.c bstack.c
-  cp $srcdir/gaplessgrid.c gaplessgrid.c
-  sed -i 's/CPPFLAGS =/CPPFLAGS +=/g' config.mk
-  sed -i 's/^CFLAGS = -g/#CFLAGS += -g/g' config.mk
-  sed -i 's/^#CFLAGS = -std/CFLAGS += -std/g' config.mk
-  sed -i 's/^LDFLAGS = -g/#LDFLAGS += -g/g' config.mk
-  sed -i 's/^#LDFLAGS = -s/LDFLAGS += -s/g' config.mk
-  make X11INC=/usr/include/X11 X11LIB=/usr/lib/X11
+  make X11INC=/usr/include/X11 X11LIB=/usr/lib/X11 FREETYPEINC=/usr/include/freetype2
 }
 
 package() {
--- config.h
+++ config.h
@@ -1,62 +1,51 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*";
-#define NUMCOLORS 3
-static const char colors[NUMCOLORS][ColLast][9] = {
-  // border foreground background
-  { "#282a2e", "#bbbbbb", "#222222" }, // 1 = default normal + yellow border
-  { "#f0c674", "#eeeeee", "#005577" }, // 2 = default selected + yellow border
-  { "#dc322f", "#1d1f21", "#f0c674" }, // 3 = urgent (black on yellow)
-  /* wongdev's color set
-  { "#282a2e", "#373b41", "#1d1f21" }, // 1 = normal (grey on black)
-  { "#f0c674", "#c5c8c6", "#1d1f21" }, // 2 = selected (white on black)
-  { "#dc322f", "#1d1f21", "#f0c674" }, // 3 = urgent (black on yellow)
-  { "#282a2e", "#282a2e", "#1d1f21" }, // 4 = darkgrey on black (for glyphs)
-  { "#282a2e", "#1d1f21", "#282a2e" }, // 5 = black on darkgrey (for glyphs)
-  { "#282a2e", "#cc6666", "#1d1f21" }, // 6 = red on black
-  { "#282a2e", "#de935f", "#1d1f21" }, // 8 = orange on black
-  { "#282a2e", "#f0c674", "#282a2e" }, // 9 = yellow on darkgrey
-  { "#282a2e", "#81a2be", "#282a2e" }, // A = blue on darkgrey
-  { "#282a2e", "#b294bb", "#282a2e" }, // B = magenta on darkgrey
-  { "#282a2e", "#8abeb7", "#282a2e" }, // C = cyan on darkgrey
-  */
+static const char *fonts[] = {
+	"monospace:size=10"
 };
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 8;        /* snap pixel */
-static const Bool showbar           = True;     /* False means no bar */
-static const Bool topbar            = True;     /* False means bottom bar */
+static const char dmenufont[]       = "monospace:size=10";
+static const char normbordercolor[] = "#444444";
+static const char normbgcolor[]     = "#222222";
+static const char normfgcolor[]     = "#bbbbbb";
+static const char selbordercolor[]  = "#005577";
+static const char selbgcolor[]      = "#005577";
+static const char selfgcolor[]      = "#eeeeee";
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
-#include "bstack.c"
-#include "gaplessgrid.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "TTT",      bstack },
-	{ "###",      gaplessgrid },
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
@@ -64,25 +53,17 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char  *dmenucmd[]     = { "dmenu_run", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
-static const char *termcmd[]  = { "urxvtc", NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *termcmd[]  = { "st", NULL };
 
-/* custom commands */
-static const char *dmenuterminalcmd[] = { "/home/yongbin/bin/dmenu_terminal", "-b", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "urxvtc", "-name", scratchpadname, "-geometry", "100x25", NULL };
-#include "push.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_o,      spawn,          {.v = dmenuterminalcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_s,      togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY|ControlMask,           XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,      pushdown,       {0} },
-	{ MODKEY|ShiftMask,             XK_k,      pushup,         {0} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
@@ -93,8 +74,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_b,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -126,8 +105,8 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        toggleview,     {0} },
-	{ ClkTagBar,            0,              Button3,        view,           {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
